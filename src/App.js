3e9c8966ffa1db0550405f96e4e8b5faf2fb1fdf
Veronica Valenzuela <vmvalenzuela24@gmail.com>
2022-03-08 18:09:32 -0800

    general layout and api call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState, useEffect } from "react";
 import "./styles.css";
+import "@fontsource/roboto/300.css";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-export default function App() {
+const quotesApi = "https://api.quotable.io/random";
+
+const App = () => {
+  const [quote, setQuote] = useState([]);
+
+  //Fetch quote from API
+  const getQuote = () => {
+    fetch(quotesApi)
+      .then((response) => response.json())
+      .then((data) => setQuote(data));
+  };
+
   return (
     <div className="App">
       <h1>Random Quote</h1>
+      <Typography variant="body1" gutterBottom>
+        {quote}
+      </Typography>
+      <Button variant="outlined" onClick={getQuote}>
+        New Quote
+      </Button>
     </div>
   );
-}
+};
+
+export default App;
